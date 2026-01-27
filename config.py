e2e1c220f37f96d7da48e07125cc6d7e05c2bcf0
sporofitabez-ux <sporofitabez@gmail.com>
2026-01-27 20:39:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,5 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-DOWNLOAD_DIR = "downloads"
-
-ARIA2_RPC_URL = "http://localhost:6800/jsonrpc"
-ARIA2_SECRET = ""  # deixe vazio
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN não definido nas variáveis de ambiente")
