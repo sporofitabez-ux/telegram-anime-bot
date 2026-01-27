e4347a23b70967103c25f13af5f22adef0c5dae4
sporofitabez-ux <sporofitabez@gmail.com>
2026-01-27 18:23:45 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,18 +1,24 @@
-import aria2p
 import os
+import requests
+import json
+from config import DOWNLOAD_DIR, ARIA2_RPC_URL, ARIA2_SECRET
 
-aria2 = aria2p.API(
-    aria2p.Client(
-        host="http://localhost",
-        port=6800,
-        secret=""
-    )
-)
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
 
-def download_link(link):
-    if link.startswith("magnet:"):
-        download = aria2.add_magnet(link)
-        return download
-    else:
-        download = aria2.add_uris([link])
-        return download
+def aria2_add(uri):
+    payload = {
+        "jsonrpc": "2.0",
+        "id": "qwer",
+        "method": "aria2.addUri",
+        "params": [
+            f"token:{ARIA2_SECRET}" if ARIA2_SECRET else [],
+            [uri],
+            {
+                "dir": DOWNLOAD_DIR
+            }
+        ]
+    }
+
+    response = requests.post(ARIA2_RPC_URL, json=payload)
+    return response.json()
