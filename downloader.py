ae691cf9aa687f55960c97f240b99d95c4afbcf1
sporofitabez-ux <sporofitabez@gmail.com>
2026-01-28 00:01:09 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,13 +1,19 @@
 import aria2p
-from config import DOWNLOAD_DIR
+import os
 
-client = aria2p.Client(
-    host="http://localhost",
-    port=6800,
-    secret=""
+DOWNLOAD_DIR = "downloads"
+
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+aria2 = aria2p.API(
+    aria2p.Client(
+        host="http://localhost",
+        port=6800,
+        secret=""
+    )
 )
 
-aria2 = aria2p.API(client)
-
-def baixar(link):
-    return aria2.add_uris([link], options={"dir": DOWNLOAD_DIR})
+def download(url):
+    download = aria2.add_uris([url], options={"dir": DOWNLOAD_DIR})
+    download.wait_for_complete()
+    return download.files[0].path
