40e0a3afb2f179095f9216c82ae27db8edcb0fd0
sporofitabez-ux <sporofitabez@gmail.com>
2026-01-27 18:22:26 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-BOT_TOKEN = "8365947886:AAE7b0qRiJiB0UCTs3X6HItVjK8INYIulJs"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DOWNLOAD_DIR = "downloads"
+
+ARIA2_RPC_URL = "http://localhost:6800/jsonrpc"
+ARIA2_SECRET = ""  # deixe vazio
